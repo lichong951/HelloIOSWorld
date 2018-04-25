ea4a07cb0a7f3acf4fb1f94c2e63d94e8225bf17
lichong <lichongmac@163.com>
2018-04-25 10:55:17 +0800

    UITextField

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 int tip=0;
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 
 @end
 
@@ -82,7 +82,36 @@ int tip=0;
 //     button.imageEdgeInsets = UIEdgeInsetsMake(- (button.frame.size.height - button.titleLabel.frame.size.height- button.titleLabel.frame.origin.y),(button.frame.size.width -button.titleLabel.frame.size.width)/2.0f -button.imageView.frame.size.width, 0, 0);
     [self.view addSubview:button];
     
+    /**=========================UITextField======================================================**/
+    UITextField *textField=[[UITextField alloc] initWithFrame:CGRectMake(20, 180, 280, 30)];
+    textField.borderStyle=UITextBorderStyleRoundedRect;
+    textField.placeholder=@"请输入文字";
+    textField.textColor=[UIColor redColor];
+    textField.font=[UIFont systemFontOfSize:14];
+    textField.textAlignment=NSTextAlignmentCenter;
+    UIImage *imageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"ic_launcher"]];
+    textField.leftView=imageView;
+    textField.leftViewMode=UITextFieldViewModeAlways;
+    textField.delegate=self;
+    [self.view addSubview:textField];
+    
 }
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    if(string.length>0){
+        if ([string characterAtIndex:0]<'0'||[string characterAtIndex:0]>'9') {
+            NSLog(@"请输入数字");
+            return NO;
+        }
+        if (textField.text.length+string.length>11) {
+            NSLog(@"超过11位啦！");
+            return NO;
+        }
+        
+    }
+    return YES;
+}
+
 -(void)changeColor{
     self.view.backgroundColor=[UIColor colorWithRed:arc4random()%255/255.0
                                               green:arc4random()%255/255.0
