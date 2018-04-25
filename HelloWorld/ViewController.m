2c154b5b8fa845504193ad979a5e800507f258ee
lichong <lichongmac@163.com>
2018-04-25 11:53:01 +0800

    UISwitch

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -95,6 +95,23 @@ int tip=0;
     textField.delegate=self;
     [self.view addSubview:textField];
     
+    /**=========================UISwitch======================================================**/
+    UISwitch *swi=[[UISwitch alloc]initWithFrame:CGRectMake(100, 220, 100, 40)];
+    swi.onTintColor=[UIColor greenColor];
+    swi.tintColor=[UIColor redColor];
+    swi.thumbTintColor=[UIColor orangeColor];
+    [swi addTarget:self action:@selector(changeBackgroundColor:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:swi];
+    
+}
+
+-(void) changeBackgroundColor:(UISwitch *)swi{
+    if (swi.isOn) {
+        self.view.backgroundColor=[UIColor redColor];
+        
+    }else{
+        self.view.backgroundColor=[UIColor whiteColor];
+    }
 }
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
