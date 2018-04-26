bd05952b2e84e5427f2fc76b8f717bc97e62b7a2
lichong <lichongmac@163.com>
2018-04-26 17:33:17 +0800

    UIAlertController

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -209,7 +209,48 @@ int tip=0;
     btn.layer.shadowOpacity=1;
     [self.view addSubview:btn];
     
+    
 }
+/**=====================================================
+ ***              UIAlertController
+ ======================================================**/
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    //警告框
+//    UIAlertController *alertView=[UIAlertController alertControllerWithTitle:@"标题" message:@"警告的内容" preferredStyle:UIAlertControllerStyleAlert];
+//    UIAlertAction *action=[UIAlertAction actionWithTitle:@"按钮" style:UIAlertActionStyleDefault handler:^(UIAlertAction *_Nonnull action){
+//        NSLog(@"click");
+//    }];
+//
+//    UIAlertAction *action2=[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction *_Nonnull action){
+//        NSLog(@"取消");
+//    }];
+//    [alertView addAction:action];
+//    [alertView addAction:action2];
+//    [alertView addTextFieldWithConfigurationHandler:^(UITextField *_Nonnull textField){
+//        textField.placeholder=@"place";
+//    }];
+//
+//    [self presentViewController:alertView animated:YES completion:nil];
+    //活动列表框
+        UIAlertController *alertView=[UIAlertController alertControllerWithTitle:@"标题" message:@"警告的内容" preferredStyle:UIAlertControllerStyleActionSheet];
+        UIAlertAction *action=[UIAlertAction actionWithTitle:@"one" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *_Nonnull action){
+            NSLog(@"one");
+        }];
+    
+        UIAlertAction *action2=[UIAlertAction actionWithTitle:@"two" style:UIAlertActionStyleDefault handler:^(UIAlertAction *_Nonnull action){
+            NSLog(@"two");
+        }];
+    UIAlertAction *action3=[UIAlertAction actionWithTitle:@"three" style:UIAlertActionStyleCancel handler:^(UIAlertAction *_Nonnull action){
+        NSLog(@"three");
+    }];
+        [alertView addAction:action];
+        [alertView addAction:action2];
+    [alertView addAction:action3];
+    
+        [self presentViewController:alertView animated:YES completion:nil];
+    
+}
+
 -(void)click:(UIStepper *) stepper{
     NSLog(@"%f",stepper.value);
 }
