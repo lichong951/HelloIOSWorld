dc2718f3d260f34aa3c26ad0130ac42dedb79aaa
lichong <lichongmac@163.com>
2018-05-02 16:41:02 +0800

    UICollectionView进行圆环布局

---
+++ HelloWorld/ui/MyUICollectionViewCircleLayout.h
@@ -0,0 +1,13 @@
+//
+//  MyUICollectionViewCircleLayout.h
+//  HelloWorld
+//
+//  Created by lichong on 2018/5/2.
+//  Copyright © 2018年 lichong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyUICollectionViewCircleLayout : UIViewController
+
+@end
---
+++ HelloWorld/view/UICollectionCircleViewFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  UICollectionCircleViewFlowLayout.m
+//  HelloWorld
+//
+//  Created by lichong on 2018/5/2.
+//  Copyright © 2018年 lichong. All rights reserved.
+//
+
+#import "UICollectionCircleViewFlowLayout.h"
+
+@implementation UICollectionCircleViewFlowLayout
+
+@end
